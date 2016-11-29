854f8eb76c59467ff545896bd0c0fbb19ce2d816
Michel Ferreira da Silva <michel.ferreira.silva@gmail.com>
2016-11-29 01:21:45 -0200

    Update Constantes.java
    Alteração constantes

--- InstaSearch/Dominio/src/main/java/br/com/tcc/Constantes/Constantes.java
+++ InstaSearch/Dominio/src/main/java/br/com/tcc/Constantes/Constantes.java
@@ -2,9 +2,9 @@ package br.com.tcc.Constantes;
 
 public final class Constantes {
 
-	public static final String TOKEN = "3410525571.9256513.84958ac8a10f42dc8d3bc8bc6c5352cb";
+	public static final String TOKEN = "SEU_TOKEN";
 	
-	public static final String CODIGO_GOIANIAINDICA = "2235325390";
+	public static final String CODIGO_GOIANIAINDICA = "CODIGO_PERFIL";
 
 	public static final int NUM_POSTS = 5;
 }

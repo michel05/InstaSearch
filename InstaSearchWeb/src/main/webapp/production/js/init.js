27281dc67093ab6f3c37b98d305ac27abcdf1736
Michel <michel.ferreira.silva@gmail.com>
2017-09-17 23:24:16 -0300

    Ajuste para aparecer a duração do monitoramento

--- InstaSearchWeb/src/main/webapp/production/js/init.js
+++ InstaSearchWeb/src/main/webapp/production/js/init.js
@@ -1,10 +1,9 @@
 
 
 $(document).ready(function() {
-//	caminho_padrao = "http://52.10.61.126:8080";
 
 	moment.locale("pt-br");
-	caminho_padrao = "http://ic1.inf.ufg.br/";
+	caminho_padrao = "http://localhost:8080";
 	inicieParametros();
 	inicio();
 
@@ -37,7 +36,7 @@ function dateJsonToTimestamp(dateJson) {
 }
 
 function padDigits(number, digits) {
-//    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
 }
 
 function inicieParametros() {
